441bfc154b251355855bdddef56b5618714beec8
yairdorantes <yairmasterlol@gmail.com>
2021-05-14 00:03:59 -0500

    vista perfecta items todo cool

--- Datos/urls.py
+++ Datos/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import view_dato,sorted
+from .views import view_dato,sorted,sorted_two
 urlpatterns = [
     path('update/',view_dato,name='update_datos'),  
     path('random',sorted,name='sorted_data'),
+    path('two/',sorted_two,name='sorted_two')
 ]
 
--- Datos/views.py
+++ Datos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import formupost
 from .models import Dato
 import random
+from django.db.models import Q
 # Create your views here.
 def view_dato(request):
     if request.method == 'POST':
@@ -19,5 +20,27 @@ def sorted(request):
     for i in datos_order:
         lista.append(i)
     random.shuffle(lista)
-    return render(request, 'Datos/index.html',{'shuffle_data':lista})
+    queryset = request.GET.get('buscar')
     
+    if queryset:
+        lista = Dato.objects.filter(
+            Q(id__icontains = queryset) | 
+            Q(autor = queryset)
+        )
+
+    return render(request, 'Datos/index.html',{'shuffle_data':lista})
+def sorted_two(request):
+    lista = []
+    datos_order = Dato.objects.all()
+    for i in datos_order:
+        lista.append(i)
+    random.shuffle(lista)
+    queryset = request.GET.get('buscar')
+    
+    if queryset:
+        lista = Dato.objects.filter(
+            Q(id__icontains = queryset) | 
+            Q(autor = queryset)
+        )
+
+    return render(request, 'Datos/two.html',{'shuffle_data':lista})
